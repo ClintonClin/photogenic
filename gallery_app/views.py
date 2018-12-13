37f4c0434d3b1170f2d2c8f7a2aa94753c839bea
ClintonClin <clintonclin00@gmail.com>
2018-12-13 03:48:51 +0300

    created views that helps in views of my templates

--- gallery_app/views.py
+++ gallery_app/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
-# Create your views here.
+from django.http import HttpResponse, Http404
+from .models import Category, Image
 
 
-def welcome(request):
-    return HttpResponse('Welcome to my Gallery-App')
+def main(request):
+    all_images = Image.objects.all()
+    categories = Category.objects.all()
+    title = 'Home'
+
+    return render(request, 'index.html', {'all_images': all_images, 'categories': categories, 'title': title})
+
+
+def search_results(request):
+    categories = Category.objects.all()
+    title = 'Search'
+
+    if 'searchcat' in request.GET and request.GET["searchcat"]:
+        search_term = request.GET.get("searchcat")
+        searched_images = Image.search_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'index.html', {"message": message, "all_images": searched_images, 'categories': categories, 'title': title})
+
+    else:
+        message = "No search item."
+        return render(request, 'index.html', {"message": message, 'categories': categories, 'title': title})
+
+
+def page_category(request, category):
+    categories = Category.objects.all()
+    title = f"{category}"
+    category_results = Image.search_category(category)
+    return render(request, 'index.html', {'all_images': category_results, 'categories': categories, 'title': title})
+
+
+
